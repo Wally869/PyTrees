c1c1183c41d9a125c37896da2298f166fcfbcce1
WillB <william.bele@edhec.com>
2021-05-18 14:22:11 +0200

    Add files via upload

--- Trie.py
+++ Trie.py
@@ -1,12 +1,12 @@
-from typing import List, Tuple, Union
+from typing import List, Tuple, Union, Dict
 
 
 from BaseTree import BaseTree
 
 class Trie(BaseTree):
     def __init__(self):
-        self.Map = {}
-        self.Value = None
+        self.Map: Dict[Trie] = {}
+        self.Value: Union[None, int] = None
     def __setitem__(self, item: str, value: int) -> None:
         if len(item) == 0:
             raise KeyError("Trie __setitem__ - Invalid item of len 0")
@@ -25,6 +25,18 @@ class Trie(BaseTree):
                 return self.Map[item].Value
             else:
                 return self.Map[item[0]][item[1:]]
+    def delete(self, item: str) -> bool:
+        if len(item) == 0:
+            raise KeyError("Trie delete - Invalid item of len 0")
+        else:
+            if self.__getitem__(item) is None:
+                return False
+            else:
+                if len(item) == 1:
+                    self.Map[item] = None
+                else:
+                    self.Map[item[0]].delete(item[1:])
+                return True
     def __ensure_not_none(self, key: str):
         if key not in self.Map.keys():
             self.Map[key] = Trie()
